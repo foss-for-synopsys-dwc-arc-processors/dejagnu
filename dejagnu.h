299b7869f1c3a33056abe9a05fffbbd4a6eaea2a
Jacob Bachmeyer <jcb@gnu.org>
2024-09-03 22:02:22 -0500

    Update most license notices to refer to WWW address
    The old GPL notice previously used in DejaGnu contained instructions to
    write to the FSF to request a copy of the GPL.  Unfortunately, the FSF
    office has moved, and the FSF now recommends citing a URL as a more
    stable address from which users can obtain a copy of the GPL if needed.
    
    This patch was automated.

--- dejagnu.h
+++ dejagnu.h
@@ -3,9 +3,9 @@
 
 This file is part of DejaGnu.
 
-DejaGnu is free software; you can redistribute it and/or modify it
+DejaGnu is free software: you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
+the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 DejaGnu is distributed in the hope that it will be useful, but
@@ -14,8 +14,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with DejaGnu; if not, write to the Free Software Foundation,
-Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.  */
+along with DejaGnu.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #ifndef __DEJAGNU_H__
 #define __DEJAGNU_H__
