a9c56ec4a94d5bae13ab5229eec7c81c1910a87a
Jacob Bachmeyer <jcb@gnu.org>
2024-09-03 22:11:33 -0500

    Tidy remaining license notices not fixed with automation
    These varied, with some being in mixed style and others incomplete.

--- contrib/mysql/create-db.sql
+++ contrib/mysql/create-db.sql
@@ -2,10 +2,18 @@
 
 -- This file is part of DejaGnu.
 
--- DejaGnu is free software; you can redistribute it and/or modify it
+-- DejaGnu is free software: you can redistribute it and/or modify it
 -- under the terms of the GNU General Public License as published by
--- the Free Software Foundation; either version 3 of the License, or
+-- the Free Software Foundation, either version 3 of the License, or
 -- (at your option) any later version.
+
+-- DejaGnu is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with DejaGnu.  If not, see <http://www.gnu.org/licenses/>.
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
